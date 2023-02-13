3042e5cbe1959ce9d15afb75fe5d40b5171b8158
Tordai, Ralph <ralph.tordai.2021@leibniz-fh.de>
2023-02-13 15:59:03 +0100

    new forms

---
+++ RessourcenErstellen.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Hochschulverwaltung
+{
+    partial class RessourcenErstellen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Flp_Main = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // Flp_Main
+            // 
+            this.Flp_Main.Location = new System.Drawing.Point(75, 75);
+            this.Flp_Main.Name = "Flp_Main";
+            this.Flp_Main.Size = new System.Drawing.Size(631, 204);
+            this.Flp_Main.TabIndex = 0;
+            // 
+            // RessourcenErstellen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Flp_Main);
+            this.Name = "RessourcenErstellen";
+            this.Text = "RessourcenErstellen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel Flp_Main;
+    }
+}
